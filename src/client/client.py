cf36cdd9095c656343d6291e7203584492ff4835
pullak404 <pullak404@gmail.com>
2026-08-05 09:12:04 +0530

    rm data

--- src/client/client.py
+++ src/client/client.py
@@ -8,31 +8,6 @@ import os
 
 from src.server.user import UserManager
 
-# HEADER = 64 # gives the length of the msg that clients will send
-# FORMAT = 'utf-8'
-# DISCONNECT_MESSAGE = "!DISCONNECT"
-
-# PORT = 9999
-# SERVER = socket.gethostbyname(socket.gethostname())
-# ADDR = (SERVER, PORT)
-
-# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# client.connect(ADDR)
-
-
-# def send(msg):
-#     message = msg.encode(FORMAT)
-#     msg_len = len(message)
-#     send_len = str(msg_len).encode(FORMAT)
-#     send_len += b' '*(HEADER - len(send_len)) # adding padding for it to be 64 bytes long
-#     client.send(send_len)
-#     client.send(message)
-
-# send("Hello World!")
-# send("Hello World!")
-# send("Hello World!")
-# send(DISCONNECT_MESSAGE)
-
 
 # ANSI Escape color codes for visual tracking inside terminal windows
 COLOR_SYS = "\033[93m"  # Yellow system notifications
